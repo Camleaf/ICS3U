1ba561e962daa3898955ebdb1a3b27f03929f245
CamLeaf <bdaace09@gmail.com>
2025-04-14 17:21:42 -0400

    add some stuf

--- U3/utils/enemies/main.py
+++ U3/utils/enemies/main.py
@@ -1,15 +1,15 @@
 import pygame as pg
-import threading
+import threading, random
 
 from ..display.colours import *
 from .enemy import Enemy
-
+from ..walls.main import Walls
 class Enemies:
     """Class which contains all the enemies"""
     units: list[Enemy]
     active_paths: list[threading.Thread]
 
-    def __init__(self, enemies_num, width, height, GAME_BASE, GAME_HEIGHT, DISPLAY_BASE, DISPLAY_HEIGHT):
+    def __init__(self, enemies_num, width, height, GAME_BASE, GAME_HEIGHT, DISPLAY_BASE, DISPLAY_HEIGHT,walls:Walls):
         
         self.number = enemies_num
         self.active_paths = []
@@ -21,17 +21,28 @@ class Enemies:
         self.DISPLAY_HEIGHT = DISPLAY_HEIGHT
         self.unit_width = width
         self.unit_height = height
-        self.create_units()
+        self.create_units(walls)
 
-    def create_units(self):
+    def create_units(self,walls:Walls):
         """Creates the enemy class self.number and stores them in a wrapper"""
         for i in range(self.number):
+            x = None
+            y = None
+            for r in range(1000):
+                ry = random.randint(0,self.GAME_HEIGHT//70 - 1)
+                rx = random.randint(0, self.GAME_BASE // 70 - 1)
+                if [rx,ry] in walls.walls:
+                    continue
+                y = ry
+                x = rx
+                break
+
             self.units.append(
                 Enemy(RED, 
                       self.unit_width, 
                       self.unit_height, 
-                      100, # xpos. Temporary until I make a placement function
-                      100, # ypos. Temporary until I make a placement function
+                      x*70 + 35, # xpos. Temporary until I make a placement function
+                      y*70 + 35, # ypos. Temporary until I make a placement function
                       self.DISPLAY_HEIGHT, 
                       self.DISPLAY_BASE, 
                       self.GAME_BASE, 
@@ -40,5 +51,5 @@ class Enemies:
     def render(self, DISPLAY, camera_x, camera_y):
         # very much a temporary render function
         for unit in self.units:
-            print(unit.x-camera_x+self.GAME_BASE//2, unit.y-camera_y+self.GAME_HEIGHT//2)
+            #print(unit.x-camera_x+self.GAME_BASE//2, unit.y-camera_y+self.GAME_HEIGHT//2)
             DISPLAY.blit(unit.image, (unit.x-camera_x+self.GAME_BASE//2, unit.y-camera_y+self.GAME_HEIGHT//2))
